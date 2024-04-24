3b99c01b51afd53720ef327d797e956c3ea8932e
srinath sriramula <sriramulasrinath59@gmail.com>
2024-04-24 12:11:11 +0530

    commiting changes

--- backend.sh
+++ backend.sh
@@ -34,7 +34,7 @@ VALIDATE $? "Extracted backend code"
 npm install &>>$LOGFILE
 VALIDATE $? "installing nodejs dependencies"
 
-cp /home/ec2-user/expense/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+cp /home/ec2-user/Example-expense/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 VALIDATE $? "Copied backend service"
 
 systemctl daemon-reload &>>$LOGFILE
